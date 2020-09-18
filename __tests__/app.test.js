5f91673929456b74c776fe79f2da6efdade65499
Hannah Riley <riley.hannahm@gmail.com>
2020-09-17 17:52:01 -0700

    updated scrape & test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,6 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
-const request = require('supertest');
-const app = require('../lib/app');
-
-describe('chorustube-be routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+describe('app.js', () => {
+  it('says hi', () => {
+    const string = 'says hi';
   });
 });
+
